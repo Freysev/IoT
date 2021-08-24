40bb4bacd90ead749bd498b2cb9bc8da0c237bfa
Vetle Rødbakk <karl.rodbakk@tietoevry.com>
2021-08-24 18:52:20 +0200

    Moist and wifi

--- MoistGudene/MoistGudene.ino
+++ MoistGudene/MoistGudene.ino
@@ -1,16 +1,41 @@
+#include <SoftwareSerial.h>
 #define SensorPin A0 
+SoftwareSerial ESPserial(2, 3); // RX | TX
+ 
 float sensorValue = 0;
+ 
+void setup()
+{
+  Serial.begin(115200); // communication with the host computer
+   
+  ESPserial.begin(115200);
+   
+  Serial.println("Ready"); 
+}
+ 
+void loop() 
+{
+  wifiRead();
+  //moistRead();
+}
 
-void setup() { 
- Serial.begin(9600); 
-} 
-void loop() { 
- for (int i = 0; i <= 100; i++) 
- { 
-   sensorValue = sensorValue + analogRead(SensorPin); 
-   delay(1); 
- } 
- sensorValue = sensorValue/100.0; 
- Serial.println(sensorValue); 
- delay(300); 
-} 
+void wifiRead (){
+  // listen for communication from the ESP8266 and then write it to the serial monitor
+   
+  if ( ESPserial.available() ) { Serial.write( ESPserial.read() ); }
+   
+  // listen for user input and send it to the ESP8266
+   
+  if ( Serial.available() ) { ESPserial.write( Serial.read() ); }
+}
+
+void moistRead(){
+  for (int i = 0; i <= 100; i++) 
+   { 
+     sensorValue = sensorValue + analogRead(SensorPin); 
+     delay(1); 
+   } 
+   sensorValue = sensorValue/100.0; 
+   Serial.println(sensorValue); 
+   delay(1000); 
+}
